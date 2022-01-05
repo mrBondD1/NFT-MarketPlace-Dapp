e3c9a2c8172e2fb91fe9a8d6c4216a38ccfa4f35
mrBondD1 <hrxbond@gmail.com>
2022-01-05 23:40:05 +0600

    updated appjs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-
-import styles from '../styles/Home.module.css'
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>NFT</h1>
     </div>
-  )
+  );
 }
